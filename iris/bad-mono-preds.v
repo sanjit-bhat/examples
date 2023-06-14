7f3d1d285df3013061426cec33072156a9b63241
Sanjit Bhat <sanjit.bhat@gmail.com>
2023-06-14 11:59:39 +0200

    bad mono preds

---
+++ iris/bad-mono-preds.v
@@ -0,0 +1,32 @@
+From iris.heap_lang Require Import proofmode notation.
+
+Section proof.
+
+Context `{!heapGS Σ}.
+
+Definition BadTransf :=
+    λ (Φ : (val → iProp Σ)),
+        (∀ v, Φ v -∗ False)%I.
+
+Lemma BadTransfMono Φ Ψ:
+    (∀ v, Φ v -∗ Ψ v) -∗ BadTransf Φ -∗ BadTransf Ψ.
+Proof.
+    iIntros "Himpl HΦ".
+    unfold BadTransf.
+    iIntros (v) "HΨ".
+    (* This is not provable.
+    Going backwards, you need Φ v0,
+    but there's no way to get that.
+    You would need (Ψ v0 -∗ Φ v0) to do it. *)
+Admitted.
+
+Lemma BadTransfNotMono Φ Ψ:
+    (∀ v, Ψ v -∗ Φ v) -∗ BadTransf Φ -∗ BadTransf Ψ.
+Proof.
+    iIntros "Himpl HΦ".
+    unfold BadTransf.
+    iIntros (v) "HΨ".
+    iApply "HΦ".
+    iApply "Himpl".
+    iAssumption.
+Qed.
