2d9a89a4e373f8042eb6f9abab34828392fefebb
Sanjit Bhat <sanjit.bhat@gmail.com>
2023-10-25 19:34:07 -0400

    x match persis

--- iris/match-persis-ts.v
+++ iris/match-persis-ts.v
@@ -10,22 +10,38 @@ Hint Extern 100 (Persistent (match ?x with _ => _ end)) =>
 
 Definition baz (b: bool) : iProp Σ :=
   if b then True%I else False%I.
-Definition foo : iProp Σ :=
+Definition bar : iProp Σ :=
+  ∃ l : list nat, match l with [] => True | _ => False end.
+Definition foo1 : iProp Σ :=
   ∃ b : bool, if b then True else False.
 Definition foo2 : iProp Σ :=
   ∀ b : bool, if b then True else False.
-Definition bar : iProp Σ :=
-  ∃ l : list nat, match l with [] => True | _ => False end.
+Definition foo3 : iProp Σ :=
+  ∀ b : bool, if b then True else False.
+Opaque foo3.
 
 (* Set Typeclasses Debug. *)
 (* Print Instances Persistent. *)
 
-Goal ∀ b, Persistent (baz b).  intros. Fail apply _. Abort.
 (*
 Doesn't trigger anything because it's not unfolding baz.
  *)
+Goal ∀ b, Persistent (baz b). intros. Fail apply _. Abort.
 
-Goal Persistent foo2. apply _. Qed.
+Goal Persistent bar. apply _. Qed.
+
+(*
+bi.exist_persistent
+First starts with forall x, Persistent ite x?
+No match for this.
+Then it looks for Persistent ite x (inner quantification).
+This transformation probably comes from the rule that lets
+us commute forall and persistent.
+After this, it finds our hint.
+Destructs and proves the branches.
+ *)
+Goal Persistent foo1. apply _. Qed.
+
 (*
 Tries bi.forall_persistent.
 No match for (BiPersistentlyForall PROP).
@@ -40,18 +56,18 @@ bi.exist_persistent :
 ∀ {PROP : bi} {A : Type} (Ψ : A → PROP),
   (∀ x : A, Persistent (Ψ x)) → Persistent (∃ x : A, Ψ x)
  *)
+Goal Persistent foo2. apply _. Qed.
 
-Goal Persistent foo. apply _. Qed.
 (*
-bi.exist_persistent
-First starts with forall x, Persistent ite x?
-No match for this.
-Then it looks for Persistent ite x (inner quantification).
-This transformation probably comes from the rule that lets
-us commute forall and persistent.
-After this, it finds our hint.
-Destructs and proves the branches.
+TC search doesn't unfold opaque defs.
+ *)
+Goal Persistent foo3. Fail apply _. Abort.
+
+(*
+Another note:
+`Hint Rewrite lemma` will cause additional rewriting and unification.
+Unification slows things down and also does somewhat silly things like
+unfolding definitions.
  *)
 
-Goal Persistent bar. apply _. Qed.
 End proof.
