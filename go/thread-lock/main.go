159f7e2f09a3faaa55891fa9e0fb43d81423e981
Sanjit Bhat <sanjit.bhat@gmail.com>
2024-02-07 14:38:28 -0500

    format thread lock

--- go/thread-lock/main.go
+++ go/thread-lock/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-    "fmt"
-    "sync"
+	"fmt"
+	"sync"
 )
 
 /*
@@ -15,26 +15,26 @@ you need not be on the same thread that Lock'd.
 */
 
 func main() {
-    var mu sync.Mutex
-    oneDone := make(chan struct{}, 1)
-    twoDone := make(chan struct{}, 1)
-    threeDone := make(chan struct{}, 1)
+	var mu sync.Mutex
+	oneDone := make(chan struct{}, 1)
+	twoDone := make(chan struct{}, 1)
+	threeDone := make(chan struct{}, 1)
 
-    go func() {
-        mu.Lock()
-        oneDone <- struct{}{}
-    }()
-    go func() {
-        <-oneDone
-        mu.Unlock()
-        twoDone <- struct{}{}
-    }()
-    go func() {
-        <-twoDone
-        mu.Lock()
-        threeDone <- struct{}{}
-    }()
+	go func() {
+		mu.Lock()
+		oneDone <- struct{}{}
+	}()
+	go func() {
+		<-oneDone
+		mu.Unlock()
+		twoDone <- struct{}{}
+	}()
+	go func() {
+		<-twoDone
+		mu.Lock()
+		threeDone <- struct{}{}
+	}()
 
-    <-threeDone
-    fmt.Println("success")
+	<-threeDone
+	fmt.Println("success")
 }
